c162b09cdc2c87cb908e414bccb841c4ed52c678
Brandon Seng Han Chan <bshchan.theone@gmail.com>
2024-04-22 12:29:02 +1200

    Add useDebounce hook for search functionality, preventing a new databse query on every keystroke

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -2,24 +2,27 @@
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
 
-  function handleSearch(term: string) {
-    console.log(`Searching... ${term}`);
-
-    const params = new URLSearchParams(searchParams);
-
-    if (term) {
-      params.set('query', term);
-    } else {
-      params.delete('query');
-    }
-    replace(`${pathname}?${params.toString()}`);
-  }
+  //function handleSearch(term: string) {
+    // only runs this code after 300 ms after the user has stopped typing.
+    const handleSearch = useDebouncedCallback((term) => {
+      console.log(`Searching... ${term}`);
+    
+      const params = new URLSearchParams(searchParams);
+      if (term) {
+        params.set('query', term);
+      } else {
+        params.delete('query');
+      }
+      replace(`${pathname}?${params.toString()}`);
+    }, 300);
+  //}
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
